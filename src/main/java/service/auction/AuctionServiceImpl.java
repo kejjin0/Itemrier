5f23162e0fefa03f4b6d5b5b29cbe6935e25b733
eunsu14 <7266706@naver.com>
2024-04-29 01:05:03 +0900

    Add: 경매 서비스 구현

--- src/main/java/service/auction/AuctionServiceImpl.java
+++ src/main/java/service/auction/AuctionServiceImpl.java
@@ -13,6 +13,35 @@ public class AuctionServiceImpl implements AuctionService{
 	@Autowired
 	private AuctionDao auctionDao;
 	
+	// 경매 상품 등록
+	public void insertAuctionItem(Auction auction) {
+        auctionDao.insertAuctionItem(auction);
+    }
+
+	// 경매 상품 수정
+    public int updateAuctionItem(Auction auction) {
+        return auctionDao.updateAuctionItem(auction);
+    }
+
+    // 경매 상품 삭제
+    public int deleteAuctionItem(int itemId) {
+        return auctionDao.deleteAuctionItem(itemId);
+    }
+
+    // 셩매 상품 조회
+    public Auction getAuctionById(int itemId) {
+        return auctionDao.getAuctionById(itemId);
+    }
+
+    // 최고 입찰가 업데이트
+    public void updateCurrentBid(int itemId, int newBid) {
+        auctionDao.updateCurrentBid(itemId, newBid);
+    }
+
+    // 경매 종료 후 현황 변경 및 낙찰자 저장
+    public void endAuction(int itemId, String status, int winner) {
+        auctionDao.endAuction(itemId, status, winner);
+    }
 	
 	///마이 페이지 위함
 	// (판매) 판매 중인 내역 확인
